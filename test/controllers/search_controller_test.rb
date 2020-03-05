e2ca9c7fafe18c767d1820fdb2824c6a34807e4d
Gabriel Britain <quontas@gmail.com>
2020-03-05 11:39:18 -0600

    Search view.

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get search_index_url
+    assert_response :success
+  end
+
 end

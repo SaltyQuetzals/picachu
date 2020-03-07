db13afd3b16411f258596575cd073533f2da94ce
Gabriel Britain <quontas@gmail.com>
2020-03-06 21:06:16 -0600

    Rubocop fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,19 @@
+# frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
   def authenticate
-  	redirect_to :login unless user_signed_in?
+    redirect_to :login unless user_signed_in?
   end
 
   def current_user
-  	@current_user ||= Authuser.find(session[:user_id]) if session[:user_id]
+    @current_user ||= Authuser.find(session[:user_id]) if session[:user_id]
   end
 
   def user_signed_in?
-  	# converts current_user to a boolean by negating the negation
-  	!!current_user
+    # converts current_user to a boolean by negating the negation
+    !!current_user
   end
-
 end

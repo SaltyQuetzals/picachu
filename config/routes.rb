5b78599ab8918dd90c4ca9eb7ecd48fe8751a9b4
Gabriel Britain <quontas@gmail.com>
2020-03-06 22:04:32 -0600

    Google login?

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'me', to: 'me#show', as: 'me'
+  get 'search' => 'search#index'
+  get 'search/professors' => 'search#professors', as: :search_professors
 end

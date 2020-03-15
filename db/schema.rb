e9480543e39bcbc93838762fe1d4316aad3219d3
Gabriel Britain <quontas@gmail.com>
2020-03-15 14:21:22 -0500

    Fixed broken stuff again.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2020_03_15_013753) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,50 +37,45 @@ ActiveRecord::Schema.define(version: 2020_03_15_013753) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.integer "overall_rating"
+    t.string "letter_grade"
+    t.string "semester"
+    t.integer "year"
+    t.boolean "course_required"
+    t.integer "interesting"
+    t.integer "difficult"
+    t.boolean "standardized_course"
+    t.string "course_other_thoughts"
+    t.boolean "used_textbook"
+    t.boolean "attendance_mandatory"
+    t.string "course_format"
+    t.integer "cared_about_material"
+    t.integer "open_to_questions"
+    t.integer "cared_about_students"
+    t.integer "clear_grading"
+    t.integer "homework_heavy"
+    t.integer "clear_explanations"
+    t.integer "fast_grading"
+    t.string "professor_other_thoughts"
+    t.bigint "professor_id"
+    t.bigint "course_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_reviews_on_course_id"
+    t.index ["professor_id"], name: "index_reviews_on_professor_id"
+  end
+
+  create_table "reviews_tags", id: false, force: :cascade do |t|
+    t.bigint "review_id", null: false
+    t.bigint "tag_id", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.integer 'overall_rating'
-    t.string 'letter_grade'
-    t.string 'semester'
-    t.integer 'year'
-    t.boolean 'course_required'
-    t.integer 'interesting'
-    t.integer 'difficult'
-    t.boolean 'standardized_course'
-    t.string 'course_other_thoughts'
-    t.boolean 'used_textbook'
-    t.boolean 'attendance_mandatory'
-    t.string 'course_format'
-    t.integer 'cared_about_material'
-    t.integer 'open_to_questions'
-    t.integer 'cared_about_students'
-    t.integer 'clear_grading'
-    t.integer 'homework_heavy'
-    t.integer 'clear_explanations'
-    t.integer 'fast_grading'
-    t.string 'professor_other_thoughts'
-    t.bigint 'professor_id'
-    t.bigint 'course_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[course_id], name: 'index_reviews_on_course_id'
-    t.index %w[professor_id], name: 'index_reviews_on_professor_id'
-  end
-
-  create_table 'reviews_tags', id: false, force: :cascade do |t|
-    t.bigint 'review_id', null: false
-    t.bigint 'tag_id', null: false
-  end
-
-  create_table 'tags', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-  end
-
-  add_foreign_key 'reviews', 'courses', on_delete: :cascade
-  add_foreign_key 'reviews', 'professors', on_delete: :cascade
+  add_foreign_key "reviews", "courses", on_delete: :cascade
+  add_foreign_key "reviews", "professors", on_delete: :cascade
 end

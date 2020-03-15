794fabb2d0893df78860b6fec76654f868bcb8c9
Gabriel Britain <quontas@gmail.com>
2020-03-15 15:23:07 -0500

    Simplecov (#21)
    * Fixed merge conflicts.
    
    * Simplecov working!
    
    * Added code coverage notes.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
-
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  puts "required simplecov"
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'

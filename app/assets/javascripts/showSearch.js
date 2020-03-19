11d9475435d2b2372c32ff9b7ce5fd8834aed95d
Grace Fan <gracefan@Graces-MacBook-Air-2.local>
2020-03-19 15:12:53 -0500

    fix search bar responsiveness

--- app/assets/javascripts/showSearch.js
+++ app/assets/javascripts/showSearch.js
@@ -1,8 +1,12 @@
-function showSearch(selectedElem) {
-    var containers = document.getElementsByClassName("search-container");
+function showSearch(elemType) {
+    containers = document.getElementsByClassName("search-container");
+    tabs = document.getElementsByClassName("radio-label");
+
     for (i = 0; i < containers.length; i++) {
       containers[i].style.display = "none";
+      tabs[i].classList.remove("active");
     }
 
-    document.getElementById(selectedElem).style.display = "block";
+    document.getElementById("search"+elemType).style.display = "block";
+    document.getElementById("label"+elemType).className += " active";
 }

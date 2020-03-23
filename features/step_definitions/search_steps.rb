2c0aad7905df9ba0552cd6c63842e1ea8ec97c0c
Gabriel Britain <quontas@gmail.com>
2020-03-22 21:48:25 -0500

    Got selenium driver working, IP not matching.

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,20 @@
+Given(/^I'm on the search page$/) do
+  visit search_path
+  puts page.body
+end
+
+When(/^I'm searching for courses$/) do
+  find(:xpath, '//*[@id="labelCourse"]').click
+end
+
+And(/^I enter "([^"]*)" into the course search bar$/) do |arg|
+  find(:xpath, '//*[@id="courseID"]').set(arg)
+end
+
+And(/^I submit my course search query$/) do
+  find(:xpath, '/html/body/div[2]/div/div[3]/form/button').click
+end
+
+Then(/^I should be redirected to the search results page$/) do
+  pending
+end

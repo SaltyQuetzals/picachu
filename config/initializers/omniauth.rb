66737568a67de1eb27dac8bd1ceb798ad6cf2e39
Sahil <sahilspalnitkar@gmail.com>
2020-03-26 15:09:10 -0500

    added line to get enviroment variables from local files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-open('.env', 'r').readlines.each {|l| kv = l.split('='); ENV[kv[0]] = kv[1];}
+Dotenv.load
   provider :google_oauth2,
            ENV['GOOGLE_CLIENT_ID'],
            ENV['GOOGLE_CLIENT_SECRET'],

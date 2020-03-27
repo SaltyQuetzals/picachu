5d63a1aeeedb4b95dcddbd2ff75e93b19d0396eb
Sahil <sahilspalnitkar@gmail.com>
2020-03-27 16:41:47 -0500

    ran prettier

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,24 +1,21 @@
 # frozen_string_literal: true
 
 Given(/^I'm on the login page/) do
-	visit root_path
+  visit root_path
 
   find(:xpath, '/html/body/div/div/a/button').click
 end
 
 Then(/^I should be redirected to the search page$/) do
-	expect(page.current_path).to eq('/auth/google_oauth2')
-	visit search_path
- 	
-    expect(page).to have_content("John Doe")
- 	find(:xpath ,'/html/body/div/a[1]').click
+  expect(page.current_path).to eq('/auth/google_oauth2')
+  visit search_path
 
- 	expect(page).to have_link("Log Out", visible: false)
+  expect(page).to have_content('John Doe')
+  find(:xpath, '/html/body/div/a[1]').click
 
- 	find(:xpath, '//*[@id="navUser"]').click
- 	find(:xpath, '//*[@id="profileDropdown"]/a').click
-	expect(page.current_path).to eq('/login')
+  expect(page).to have_link('Log Out', visible: false)
 
-
+  find(:xpath, '//*[@id="navUser"]').click
+  find(:xpath, '//*[@id="profileDropdown"]/a').click
+  expect(page.current_path).to eq('/login')
 end
-	

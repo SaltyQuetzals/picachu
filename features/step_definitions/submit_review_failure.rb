40dd27f224af7303f6d821f5e21b806e634e06f0
Deshantar Parajuli <deshantar.parajuli99@gmail.com>
2020-03-27 16:38:32 -0500

    cleaned bdd tests

--- features/step_definitions/submit_review_failure.rb
+++ features/step_definitions/submit_review_failure.rb
@@ -1,69 +1,9 @@
-Given(/^I am on the new review page$/) do
-  visit new_review_path
-end
-
 And(/^I don't select a professor from the professor menu$/) do
   find('#professor_id').find(:xpath, 'option[1]').select_option
 end
 
 And(/^I dont' select a course from the course menu$/) do
   find('#course_id').find(:xpath, 'option[1]').select_option
-end
-
-And(/^I fill in "([^"]*)" in the "Overall rating" field$/) do |value|
-  page.choose(:review_overall_rating_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Letter grade" field$/) do |value|
-  find(:xpath, '//*[@id="review_letter_grade"]').set(value)
-end
-
-And(/^I fill in "([^"]*)" in the "Semester" field$/) do |value|
-  find(:xpath, '//*[@id="review_semester"]').set(value)
-end
-
-And(/^I fill in "([^"]*)" in the "Year" field$/) do |value|
-  find(:xpath, '//*[@id="review_year"]').set(value)
-end
-
-And(/^I fill in "([^"]*)" in the "Interesting" field$/) do |value|
-  page.choose(:review_interesting_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Difficult" field$/) do |value|
-  page.choose(:review_difficult_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Cared about material" field$/) do |value|
-  page.choose(:review_cared_about_material_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Open to questions" field$/) do |value|
-  page.choose(:review_open_to_questions_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Cared about students" field$/) do |value|
-  page.choose(:review_cared_about_students_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Clear grading" field$/) do |value|
-  page.choose(:review_clear_grading_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Homework heavy" field$/) do |value|
-  page.choose(:review_homework_heavy_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Clear explanations" field$/) do |value|
-  page.choose(:review_clear_explanations_1)
-end
-
-And(/^I fill in "([^"]*)" in the "Fast grading" field$/) do |value|
-  page.choose(:review_fast_grading_1)
-end
-
-And (/^I submit the Review form/) do
-  click_button("commit")
 end
 
 Then (/^I should be redirected to the new Review page/) do
--- features/submit_review_failure.feature
+++ features/submit_review_failure.feature
@@ -6,18 +6,18 @@ Scenario: Submit a review without filling out required fields
     And I am on the new review page
     And I don't select a professor from the professor menu
     And I don't select a course from the course menu
-    And I fill in "5" in the "Overall rating" field
-    And I fill in "" in the "Letter grade" field
-    And I fill in "" in the "Semester" field
-    And I fill in "" in the "Year" field
-    And I fill in "" in the "Interesting" field
-    And I fill in "" in the "Difficult" field
-    And I fill in "" in the "Cared about material" field
-    And I fill in "" in the "Open to questions" field
-    And I fill in "" in the "Cared about students" field
-    And I fill in "" in the "Clear grading" field
-    And I fill in "" in the "Homework heavy" field
-    And I fill in "" in the "Clear explanations" field
-    And I fill in "" in the "Fast grading" field 
-    When I submit the Review form
+    And I enter "5" in the "Overall rating" field
+    And I enter "" in the "Letter grade" field
+    And I enter "" in the "Semester" field
+    And I enter "" in the "Year" field
+    And I enter "" in the "Interesting" field
+    And I enter "" in the "Difficult" field
+    And I enter "" in the "Cared about material" field
+    And I enter "" in the "Open to questions" field
+    And I enter "" in the "Cared about students" field
+    And I enter "" in the "Clear grading" field
+    And I enter "" in the "Homework heavy" field
+    And I enter "" in the "Clear explanations" field
+    And I enter "" in the "Fast grading" field 
+    When I submit my Review form
     Then I should be redirected to the new Review page

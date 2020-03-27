11a70d7b5cef9bc01935ea42670fb8dcc27da394
Sandesh Neupane <sandesh.neupane4@gmail.com>
2020-03-26 23:25:43 -0500

    updated bdd

--- features/step_definitions/report_review_steps.rb
+++ features/step_definitions/report_review_steps.rb
@@ -1,86 +1,47 @@
 # frozen_string_literal: true
 
-# Given(/^I'm viewing a specific review$/) { pending }
-
-# When(/^I click the report button$/) { pending }
-
-# And(/^I indicate that the review is spam or abusive$/) { pending }
-
-# And(/^I submit my report$/) { pending }
-
-# Then(/^I should receive confirmation that my report was successful$/) do
-#   pending
-# end
-
-# And(/^I indicate that the review is inappropriate or offensive$/) { pending }
-
-# And(/^I indicate that the review should be reported for other reasons$/) do
-#   pending
-# end
-
-# And(/^I indicate it is "([^"]*)"$/) { |arg| }
-
-# Then(/^I should get a notification that my report failed$/) { pending }
-
-
-def fill_log_in_form
-    fill_in 'tamu_email', with: 'xxx.tamu.edu'
-    fill_in 'tamu_password', with: 'xxxtamupassword'
+Given(/^I'm viewing a specific review$/) do
+    visit show_review_path
 end
 
-Given("I'm viewing a specific review") do
-    visit professor_path
+When(/^I click the report button$/) do
+	click_button("open-modal-btn")
 end
 
-When("I choose the professor") do
-    click_link 'show'
-    visit professor_show_path
+And(/^I indicate that the review is spam or abusive$/) do |arg|
+	page.choose(:reason_spamAbusive) 
 end
 
-Then("I should see reviews for selected professor") do
-    click_link 'show'
-    visit review_show_path
+And(/^I submit my report$/) do
+	click_button("reportReview")
 end
 
-AND("I should see report review click_button") do
-	click_button "report review"
+Then(/^I should receive confirmation that my report was successful$/) do
+	expect("report success notification")
+	expect(page.current_path).to eq(review_path(@review))
 end
 
-When("I click the report button") do
-    expect("three radio buttons")
+And(/^I click the report button$/) do
+	click_button("open-modal-btn")
 end
 
-Then("I indicate that the review is spam or abusive") do
-	click_button "submit"
+And(/^I indicate that the review is inappropriate or offensive$/) do |arg|
+	page.choose(:reason_inappropriateOffensive) 
 end
 
-Then("I should receive confirmation that my report was successful") do
-    click_button "ok"
+And(/^I indicate that the review should be reported for other reasons$/) do |arg|
+	page.choose(:reason_other)
 end
 
-Then("I should be in the review page") do
-  expect(page).to have_link('back')
+And(/^I indicate it is "([^"]*)"$/) do |arg|
+    find(:xpath, '//*[@id="other_input"]').set(arg)
 end
 
-AND("I indicate that the review should be reported for other reasons") do
-	expect("input text box to be active")
+Then(/^I should get a notification that my report failed$/) do
+	expect("report failure notification")
 end
 
-When("I click the submit button") do
-    expect("required notification in text input")
-end
 
-Then("I added 'it is vulgar' in input box") do
-	click_button "submit"
-end
-
-Then("I should receive confirmation that my report was successful") do
-    click_button "ok"
-end
-
-Then("I should be in the review page") do
-  expect(page).to have_link('back')
-end
 
 
 

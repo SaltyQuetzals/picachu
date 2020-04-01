24e20dd5d617184714586cbf4a2b4471389d5d79
TQ LIU <Ken@TQdeMacBook-Pro.local>
2020-04-01 13:21:45 -0500

    Update on the all course formating

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -3,6 +3,7 @@
 class ProfessorsController < ApplicationController
   before_action :set_professor, only: %i[show edit update destroy]
   before_action :set_course, only: %i[show_course]
+  helper_method :avg_rev
   # GET /professors
   # GET /professors.json
   def index
@@ -11,12 +12,21 @@ class ProfessorsController < ApplicationController
 
   # GET /professors/1
   # GET /professors/1.json
+  def avg_rev (course)
+      if course.reviews.blank?
+        0
+      else
+        course.reviews.average(:overall_rating).round(2)
+      end
+  end
+
+  
   def show
     @avg_review =
       if @professor.reviews.blank?
         0
       else
-        @professor.reviews.average(:overall_rating)
+        @professor.reviews.average(:overall_rating).round(2)
       end
 
     @num_reviews = @professor.reviews.length # does not work
@@ -42,11 +52,18 @@ class ProfessorsController < ApplicationController
         @lowest_rated_course.reviews.order('overall_rating DESC').first
     end
 
-    @highest_rating = @highest_rated_course_review.overall_rating.floor
-    @highest_rating_compo = 5 - @highest_rating
-    @lowest_rating = @lowest_rated_course_review.overall_rating.floor
-    @lowest_rating_compo = 5 - @lowest_rating
-
+    @highest_rating = 0
+    @highest_rating_compo = 5
+    @lowest_rating = 0
+    @lowest_rating_compo = 5
+    if(!@highest_rated_course_review.blank?)
+      @highest_rating = @highest_rated_course_review.overall_rating.floor
+      @highest_rating_compo = 5 - @highest_rating
+    end
+    if(!@lowest_rated_course_review.blank?)
+      @lowest_rating = @lowest_rated_course_review.overall_rating.floor
+      @lowest_rating_compo = 5 - @lowest_rating
+    end
     # Need overall review rating of particular course caught by the professor
   end
 

617df43ee1506069b9f5663947e8e70577f5c7b2
Sahil <sahilspalnitkar@gmail.com>
2020-04-02 13:08:11 -0500

    fixed test for sessions_controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,32 +1,59 @@
-# frozen_string_literal: true
-
 # sessions_controller_spec.rb
 require 'spec_helper'
 require 'rails_helper'
 RSpec.describe SessionsController do
-  describe '#create' do
-    it 'should successfully create a session' do
-      request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:google]
-
-      session[:user_id].should be_nil
-    end
-
-    it 'should redirect the user to the search page' do
-      response.should be_successful
-    end
+ 
+    auth = {
+      'provider' => 'google',
+      'uid' => '12345',
+      'info' => {
+        'name' => 'John Doe',
+        'email' => 'johndoe@doe.com',
+        'location' => 'Doe World',
+        'image' => 'image_url'
+      },
+      'extra' => { 'raw_info' => { 'hd' => '@tamu.edu' } }
+    }
+  before do
+    request.env['omniauth.auth'] = auth
   end
-
-  describe '#destroy' do
-    before { '#create' }
-
-    it 'should clear the session' do
+ 
+  describe "#create" do
+ 
+    it "should successfully create a user" do
+      expect {
+        post :create
+      }.to change{ Authuser.count }.by(1)
+    end
+ 
+    it "should successfully create a session" do
+      session[:user_id].should be_nil
+      post :create
+      session[:user_id].should_not be_nil
+    end
+ 
+    it "should redirect the user to the root url" do
+      post :create
+      response.should redirect_to search_path
+    end
+ 
+  end
+ 
+  describe "#destroy" do
+    before do
+      post :create
+    end
+ 
+    it "should clear the session" do
+      session[:user_id].should_not be_nil
       delete :destroy
       session[:user_id].should be_nil
     end
-
-    it 'should redirect to the home page' do
+ 
+    it "should redirect to the home page" do
       delete :destroy
-      response.should redirect_to :login
+      response.should redirect_to login_path
     end
   end
+ 
 end

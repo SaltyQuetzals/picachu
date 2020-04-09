eddc93b87a821381fd66b34ef8fe9d082b6f0a42
Sandesh Neupane <sandesh.neupane4@gmail.com>
2020-04-09 10:58:22 -0500

    testing

--- features/step_definitions/report_review_steps.rb
+++ features/step_definitions/report_review_steps.rb
@@ -4,25 +4,23 @@ Given(/^I'm viewing a specific review$/) { visit review_path(@review) }
 
 When(/^I click the report button$/) { click_button('open-modal-btn') }
 
-When(/^I indicate that the review is spam or abusive$/) { page.choose('spam') }
+And(/^I indicate that the review is spam or abusive$/) { page.choose('spam') }
 
-When(/^I submit my report$/) { click_button('reportReview') }
+And(/^I submit my report$/) { click_button('reportReview') }
 
 Then(/^I should receive confirmation that my report was successful$/) do
   expect(page.current_path).to eq(review_path(@review))
 end
 
-Given(/^I'm viewing a specific review$/) { visit review_path(@review) }
-
-When(/^I indicate that the review is inappropriate or offensive$/) do
+And(/^I indicate that the review is inappropriate or offensive$/) do
   page.choose('inappropriate')
 end
 
-When(/^I indicate that the review should be reported for other reasons$/) do
+And(/^I indicate that the review should be reported for other reasons$/) do
   page.choose('other')
 end
 
-When(/^I indicate it is "([^"]*)"$/) { |arg| find('#reasonForOther').set(arg) }
+And(/^I indicate it is "([^"]*)"$/) { |arg| find('#reasonForOther').set(arg) }
 
 Then(/^I should get a notification that my report failed$/) do
   expect(page.current_path).to eq(review_path(@review))

4dd3c2cdf1de1714a554b4e9862146e4a1d23038
Sahil Palnitkar <43481948+sahilpalnitkar@users.noreply.github.com>
2020-04-21 17:37:44 -0500

    removed links to sign up and edit/destroy link (#63)
    * removed links to sign up and edit/destroy link
    
    * readded test that failed without avg_review fix
    
    * added exclusions to routes
    
    * edited line in routes
    
    * Prettier fix.
    
    Co-authored-by: Gabriel Britain <quontas@gmail.com>

--- spec/requests/professors_request_spec.rb
+++ spec/requests/professors_request_spec.rb
@@ -13,29 +13,6 @@ RSpec.describe 'Professors', type: :request do
     expect(response).to have_http_status(:success)
   end
 
-  it 'should get the new page' do
-    get new_professor_url
-    expect(response).to have_http_status(:success)
-  end
-
-  it 'should create a new professor' do
-    expect {
-      post professors_url,
-           params: {
-             professor: {
-               cv_url: @professor.cv_url,
-               display_name: @professor.display_name,
-               first_name: @professor.first_name,
-               full_name: @professor.full_name,
-               last_name: @professor.last_name,
-               tamu_dir_dept: @professor.tamu_dir_dept,
-               tamu_dir_email: @professor.tamu_dir_email,
-               tamu_dir_title: @professor.tamu_dir_title
-             }
-           }
-    }.to change { Professor.count }.by(1)
-  end
-
   it 'should get the show page' do
     get professor_url(@professor)
     expect(response).to have_http_status(:success)
@@ -50,34 +27,6 @@ RSpec.describe 'Professors', type: :request do
     second_professor = professors(:two)
     get professor_url(second_professor)
     expect(response).to have_http_status(:success)
-  end
-
-  it 'should get the edit page' do
-    get edit_professor_url(@professor)
-    expect(response).to have_http_status(:success)
-  end
-
-  it 'should update a professor' do
-    patch professor_url(@professor),
-          params: {
-            professor: {
-              cv_url: @professor.cv_url,
-              display_name: @professor.display_name,
-              first_name: @professor.first_name,
-              full_name: @professor.full_name,
-              last_name: @professor.last_name,
-              tamu_dir_dept: @professor.tamu_dir_dept,
-              tamu_dir_email: @professor.tamu_dir_email,
-              tamu_dir_title: @professor.tamu_dir_title
-            }
-          }
-    expect(response).to redirect_to(professor_url(@professor))
-  end
-
-  it 'should destroy a professor' do
-    expect { delete professor_url(@professor) }.to change {
-      Professor.count
-    }.by(-1)
   end
 
   it 'should get the search page' do
